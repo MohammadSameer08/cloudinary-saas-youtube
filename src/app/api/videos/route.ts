bf55f47c7edbd9e573c3bde4b8cadb5bb5dadf01
Md Sameer <ms149132@gmail.com>
2026-06-27 15:38:08 +0530

    added prism and neon Db

---
+++ src/app/api/videos/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "../../../../prisma/generated/client";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export async function GET(request: NextRequest) {
+  try {
+    const videos = await prisma.video.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+    return NextResponse.json(videos);
+  } catch (error) {
+    console.error("Error fetching videos:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch videos" },
+      { status: 500 },
+    );
+  }
+}
